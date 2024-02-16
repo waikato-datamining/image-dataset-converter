3db3fe06e497e37bc9c5416c80a797517a575f7b
Peter Reutemann <fracpete@gmail.com>
2024-02-16 14:50:48 +1300

    annotations can be absolute or normalized now

--- src/idc/base/_objdet.py
+++ src/idc/base/_objdet.py
@@ -1,11 +1,21 @@
 from dataclasses import dataclass
+from typing import Union
 
 from ._data import ImageData
-from wai.common.adams.imaging.locateobjects import LocatedObjects
+from wai.common.adams.imaging.locateobjects import LocatedObjects, NormalizedLocatedObjects
 
 
 @dataclass
 class ObjectDetectionData(ImageData):
 
-    annotation: LocatedObjects = None
+    annotation: Union[LocatedObjects, NormalizedLocatedObjects] = None
     """ the annotations. """
+
+    def is_normalized(self) -> bool:
+        """
+        Returns whether the annotations are normalized or absolute.
+
+        :return: True if normalized
+        :rtype: bool
+        """
+        return isinstance(self.annotation, NormalizedLocatedObjects)
