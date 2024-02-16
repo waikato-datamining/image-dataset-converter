a8653a9d9fe9a895d20bf8ff5edb6db1f5a48a35
Peter Reutemann <fracpete@gmail.com>
2024-02-16 15:01:08 +1300

    added methods for converting annotations (in-place) to absolute/normalized

--- src/idc/base/_objdet.py
+++ src/idc/base/_objdet.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from ._data import ImageData
 from wai.common.adams.imaging.locateobjects import LocatedObjects, NormalizedLocatedObjects
+from wai.common.adams.imaging.locateobjects import absolute_to_normalized, normalized_to_absolute
 
 
 @dataclass
@@ -19,3 +20,21 @@ class ObjectDetectionData(ImageData):
         :rtype: bool
         """
         return isinstance(self.annotation, NormalizedLocatedObjects)
+
+    def to_normalized(self):
+        """
+        Turns the absolute annotations into normalized ones (in-place).
+        """
+        if self.is_normalized():
+            return
+        width, height = self.image_size()
+        self.annotation = absolute_to_normalized(self.annotation, width, height)
+
+    def to_absolute(self):
+        """
+        Turns the normalized annotations into absolute ones (in-place).
+        """
+        if not self.is_normalized():
+            return
+        width, height = self.image_size()
+        self.annotation = normalized_to_absolute(self.annotation, width, height)
