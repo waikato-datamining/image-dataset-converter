387a3e818bb16067ea414af20ee152d5fda3db4b
Peter Reutemann <fracpete@gmail.com>
2024-03-25 16:40:04 +1300

    improved ImageDraw.textsize handling

--- src/idc/api/_fonts.py
+++ src/idc/api/_fonts.py
@@ -7,6 +7,8 @@ from PIL import ImageFont, Image, ImageDraw
 
 DEFAULT_FONT_FAMILY = "sans\\-serif"
 
+TEXT_SIZE_OLD = None
+
 
 def load_font(logger, family, size):
     """
@@ -36,6 +38,41 @@ def load_font(logger, family, size):
         return ImageFont.truetype(font_file, size)
 
 
+def _text_size_old(text: str, font) -> Tuple[int, int]:
+    """
+    Computes the width/height of a text using the specified Pillow font.
+    Uses ImageDraw.textsize.
+
+    :param text: the text to get the dimensions for
+    :type text: str
+    :param font: the Pillow font to use
+    :return: the width and height
+    :rtype: tuple
+    """
+    im = Image.new(mode="P", size=(0, 0))
+    draw = ImageDraw.Draw(im)
+    w, h = draw.textsize(text, font=font)
+    return w, h
+
+
+def _text_size_new(text: str, font) -> Tuple[int, int]:
+    """
+    Computes the width/height of a text using the specified Pillow font.
+    Newer versions of Pillow deprecated ImageDraw.textsize
+    https://levelup.gitconnected.com/how-to-properly-calculate-text-size-in-pil-images-17a2cc6f51fd
+
+    :param text: the text to get the dimensions for
+    :type text: str
+    :param font: the Pillow font to use
+    :return: the width and height
+    :rtype: tuple
+    """
+    ascent, descent = font.getmetrics()
+    w = font.getmask(text).getbbox()[2]
+    h = font.getmask(text).getbbox()[3] + descent
+    return w, h
+
+
 def text_size(text: str, font) -> Tuple[int, int]:
     """
     Computes the width/height of a text using the specified Pillow font.
@@ -46,7 +83,18 @@ def text_size(text: str, font) -> Tuple[int, int]:
     :return: the width and height
     :rtype: tuple
     """
-    im = Image.new(mode="P", size=(0, 0))
-    draw = ImageDraw.Draw(im)
-    _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
-    return width, height
+    global TEXT_SIZE_OLD
+
+    if TEXT_SIZE_OLD is None:
+        try:
+            w, h = _text_size_old(text, font)
+            TEXT_SIZE_OLD = True
+        except:
+            w, h = _text_size_new(text, font)
+            TEXT_SIZE_OLD = False
+    else:
+        if TEXT_SIZE_OLD:
+            w, h = _text_size_old(text, font)
+        else:
+            w, h = _text_size_new(text, font)
+    return w, h
