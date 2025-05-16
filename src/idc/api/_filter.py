7345c0c2a4049b855e0a90656cc7d8794d959bed
Peter Reutemann <fracpete@gmail.com>
2025-05-16 13:58:09 +1200

    add_apply_to_param allows defining the default value now

--- src/idc/api/_filter.py
+++ src/idc/api/_filter.py
@@ -12,15 +12,17 @@ APPLY_TO = [
 ]
 
 
-def add_apply_to_param(parser: argparse.ArgumentParser):
+def add_apply_to_param(parser: argparse.ArgumentParser, default: str = APPLY_TO_IMAGE):
     """
     Adds the -a/--apply_to parameter to the parser.
 
     :param parser: the parser to add the option to
     :type parser: argparse.ArgumentParser
+    :param default: the default value
+    :type default: str
     """
     parser.add_argument("-a", "--apply_to", choices=APPLY_TO, help="Where to apply the filter to.",
-                        default=APPLY_TO_IMAGE, required=False)
+                        default=default, required=False)
 
 
 def parse_filter(filter_: str) -> Filter:
