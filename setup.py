d3718524b9412a641e0ea07042eb13a06123794a
Peter Reutemann <fracpete@gmail.com>
2025-11-13 10:12:28 +1300

    removed "License" classifier (deprecated)

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     url="https://github.com/waikato-datamining/image-dataset-converter",
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Image Processing',
     ],
